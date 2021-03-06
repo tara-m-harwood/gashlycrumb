c6b8de56e4f26a99867e20f499d9f800a970dad3
Tara Harwood <tara.m.harwood@gmail.com>
2021-03-06 12:57:43 -0800

    changing routes and form inputs

--- public/tiny.js
+++ public/tiny.js
@@ -5,10 +5,13 @@ const $tinyContainer = document.querySelector("#tiny-container")
 const queryParams = window.location.search
 const queryParamsObject = new URLSearchParams(queryParams)
 
-console.log(queryParamsObject)
-
 const endpoint = queryParamsObject.get("endpoint")
+
+console.log(endpoint)
+
 const endparam = queryParamsObject.get("endparam")
+
+console.log(endparam)
 
 
 
@@ -16,13 +19,13 @@ const endparam = queryParamsObject.get("endparam")
 fetch(`${baseURL}${endpoint}/${endparam}`)
     .then(response => response.json())
     // .then(console.log)
-    // // .then(console.log(`${baseURL}${endpoint}/${endparam}`))
-    .then(tiny => {
-        const $tinyCard = document.createElement('div')
+    .then(console.log(`${baseURL}${endpoint}/${endparam}`))
+    // .then(tiny => {
+    //     const $tinyCard = document.createElement('div')
 
-        const $tinyTitle = document.createElement('h1')
-        $tinyTitle.textContent = `You are ${tiny.name}`
+    //     const $tinyTitle = document.createElement('h1')
+    //     $tinyTitle.textContent = `You are ${tiny.name}`
 
-        $tinyCard.append($tinyTitle)
-        $tinyContainer.append($tinyCard)
-    })
+    //     $tinyCard.append($tinyTitle)
+    //     $tinyContainer.append($tinyCard)
+    // })
