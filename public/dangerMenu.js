ccd9abe128da371f90aa8d4cd3cc904ef554bb11
Tara Harwood <tara.m.harwood@gmail.com>
2021-03-06 18:57:04 -0800

    minor fixes to heroku

--- public/dangerMenu.js
+++ public/dangerMenu.js
@@ -3,6 +3,7 @@ $selectContainer = document.querySelector('#fate-select')
 fetch('https://gashlycrumb-randomly-creepy.herokuapp.com/tinies/')
   .then(response => response.json())
   .then( tinies => {tinies.forEach(tiny => {
+      console.log(tiny)
       $selectOption = document.createElement('option')
       $selectOption.className = 'fate-select-option'
       $selectOption.value = tiny.id
