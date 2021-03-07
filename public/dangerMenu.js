65e1c9590056cd4a52740e9e813329c73ccb7ca8
Tara Harwood <tara.m.harwood@gmail.com>
2021-03-06 16:08:38 -0800

    looks ok on mobile

--- public/dangerMenu.js
+++ public/dangerMenu.js
@@ -1,10 +1,10 @@
-$selectContainer = document.querySelector( '#danger-select' )
+$selectContainer = document.querySelector( '#danger-select-endparam' )
 
 fetch('https://gashlycrumb-randomly-creepy.herokuapp.com/tinies/')
   .then(response => response.json())
   .then( tinies => {tinies.forEach(tiny => {
       $selectOption = document.createElement('option')
-      $selectOption.className = 'danger-select'
+      $selectOption.className = 'danger-select-options'
       $selectOption.value = tiny.id
       $selectOption.innerText = tiny.danger
       $selectContainer.append($selectOption)
